3026ecd3d6b704c52db45fa13d1f5386e060722d
Amankumar021 <aman706839@gmail.com>
2024-07-09 10:52:59 +0000

    container config done

---
+++ javascipt/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14435
+let accountEmail = "aman@gog.com"
+var accountPassword = "1234512"
+accountCity = "Allahabad"
+
+// accountId = 2 //not allowed
+accountEmail = "ha@ha.com"
+accountPassword = 21356
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
